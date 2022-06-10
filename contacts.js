9f312c61247b9e832699423e0dd8389e2e247d2a
Akim Grakovskiy <aakim5586@gmail.com>
2022-06-10 20:47:45 +0300

    10.06.2022 - something is ready, something not

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+const path = require('path');
+const shortid = require('shortid');
+
+const contactsPath  = path.join(__dirname, "/db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const existingContacts = await listContacts();
+  const result = existingContacts.find(contact => contact.id === `${contactId}`);
+  if (!result) {
+    return null;
+  }
+  
+  return result;
+}
+
+async function removeContact(contactId) {
+  const existingContacts = await listContacts();
+  const existingIndex = existingContacts.findIndex(contact => contact.id === `${contactId}`);
+  if (existingIndex < 0) {
+    return null;
+  }
+  const remainingContacts = existingContacts.filter(contact => contact.id !== `${contactId}`);
+  await fs.writeFile(contactsPath, JSON.stringify(remainingContacts));
+  return existingContacts[existingIndex];
+}
+
+async function addContact(data) {
+  const existingContacts = await listContacts();
+  const newContact = {
+    id: shortid.generate(),
+    ...data,
+  }
+  const newContactsArray = [...existingContacts, newContact];
+  await fs.appendFile(contactsPath, newContactsArray);
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
- 
+const yargs = require("yargs");
+const {hideBin} = require("yargs/helpers");
+const {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+} = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            const contactsList = await listContacts();
+            console.log(contactsList);
+            break;
+        
+        case 'get':
+            const contactToGet = await getContactById(id);
+            if(!contactToGet){
+                throw new Error(`Contact was not found. Id "${id}" does not exist`);
+            }
+            console.log(contactToGet);
+            break;
+
+        case 'add':
+            const contactToAdd = await addContact({name, email, phone});
+            console.log(contactToAdd);
+            break;
+
+        case 'remove':
+            const contactToRemove = await removeContact(id);
+            if(contactToRemove === null){
+                throw new Error(`Contact was not found. Id "${id}" does not exist, or was deleted earlier`);
+            }
+            console.log(contactToRemove);
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+const arr = hideBin(process.argv);
+
+const {argv} = yargs(arr);
+
+invokeAction(argv);
